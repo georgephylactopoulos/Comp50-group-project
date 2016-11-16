ebc8e39a29f64b2ae69d8573bb355627de5c9211
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2016-11-16 16:05:07 -0500

    code example for trying out ets and dets

---
+++ try_ets.erl
@@ -0,0 +1,25 @@
+
+% Trying out dets and ets
+
+% Makes table_name a global
+Table = ets:new(table_name, [{write_concurrency, true}, {read_concurrency, true}]).
+
+ets:insert(Table, {"Hello", "World"}).
+
+
+% Create/Open the dets_table dets file
+{ok, DiskTable} = dets:open_file(dets_table, []).
+
+% Load and unload 
+ets:to_dets(Table, DiskTable).
+ets:from_dets(Table, DiskTable).
+
+ets:lookup(Table, "Hello").
+
+% Update value of the "Hello" key
+dets:insert(dets_table, [{"Hello", "newValue"}]).
+ets:from_dets(Table, DiskTable).
+
+ets:lookup(Table, "Hello").
+
+
