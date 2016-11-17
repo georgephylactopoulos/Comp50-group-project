136b19330521a5e2c4e09829da446002c2ba988a
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2016-11-17 16:06:39 -0500

    dets/ets-ify tablet_server

--- tablet_server.erl
+++ tablet_server.erl
@@ -7,82 +7,55 @@
 % InactiveDictFilename = "lsadkjflkajf.txt".
 
 % TODO figure out if this needs anything
-start_server() -> 
-	ActivePid = spawn(fun () -> active_loop(dict:new(), dict:new(), 0)  end),
-	InactivePid = spawn(fun () -> inactive_loop() end),
-	spawn(fun () -> loop(ActivePid, InactivePid) end).
+start_server() ->
+	ActiveTableMem = ets:new(active_table_mem, [{write_concurrency, true}, {read_concurrency, true}]),
+	{ok, ActiveTableDisk} = dets:open_file(active_table_disk, []),
+	ets:from_dets(ActiveTableMem, ActiveTableDisk),
+	{ok, InactiveTable} = dets:open_file(inactive_table_disk, []),
+	spawn(fun () -> loop(ActiveTableMem, ActiveTableDisk, InactiveTable, 0)  end).
 
-active_loop(ActiveDict, DiskActiveDict, CurrentReaders) ->
+% % Tablet server loop
+loop(ActiveTableMem, ActiveTableDisk, InactiveTable, CurrentReaders) ->
 	receive
-		{queryR, Function} ->
-			This = self(),
-			spawn(	fun () ->
-						dict:map(Function, ActiveDict),
-						This ! done_reading
-					end),
-			active_loop(ActiveDict, DiskActiveDict, CurrentReaders + 1);
+		{queryR, Function} -> todo;
+			% This = self(),
+			% spawn(	fun () ->
+			% 			dict:map(Function, ActiveDict),
+			% 			This ! done_reading
+			% 		end),
+			% loop(ActiveDict, DiskActiveDict, CurrentReaders + 1);
 		% Function has type (Key, Value) -> Value
-		{queryW, Function} ->
+		{queryW, Function} -> todo;
+			% _ = wait_for_readers(CurrentReaders),
+			% NewActiveDict = dict:map(Function, ActiveDict),
+			% loop(NewActiveDict, DiskActiveDict, 0);
+		{addInactiveRow, Key, Value} -> dets:insert(InactiveTable, [{Key, Value}]);
+		{addActiveRow, Key, Value} ->
 			_ = wait_for_readers(CurrentReaders),
-			NewActiveDict = dict:map(Function, ActiveDict),
-			active_loop(NewActiveDict, DiskActiveDict, 0);
-		{addActiveRow, Key, Value} -> 
-			NewActiveDict = dict:append(Key, Value, ActiveDict),
-			io:write(dict:to_list(NewActiveDict)),
-			active_loop(NewActiveDict, DiskActiveDict, CurrentReaders);
-		% {deleteRow, Key} ->
-		% 	NewActiveDict = dict:erase(Key, ActiveDict),
-		% 	active_loop(NewActiveDict, DiskActiveDict, CurrentReaders);
-		{quit} -> ok
+			ets:insert(ActiveTableMem, [{Key, Value}]);
+		{deleteInactiveRow, Key} ->
+			spawn(fun() ->  dets:delete(InactiveTable, Key) end), ok;
+		{deleteActiveRow, Key} -> ets:delete(ActiveTableMem, Key);
+		{updateInactiveRowValue, Key, Value} ->
+			spawn(fun() ->  dets:insert(InactiveTable, [{Key, Value}]) end), ok;
+		{makeRowActive, Key} -> 
+			_ = wait_for_readers(CurrentReaders),
+			ets:insert(ActiveTableMem, dets:lookup(InactiveTable, Key)),
+			dets:delete(InactiveTable, Key);
+		{makeRowInactive, Key} -> 
+			_ = wait_for_readers(CurrentReaders),
+			dets:insert(InactiveTable, ets:lookup(ActiveTableMem, Key)),
+			ets:delete(ActiveTableMem, Key)
 	after
 		% this refresh timer is redundant
 		5000 ->
 			case ActiveDict of
-				DiskActiveDict -> active_loop(DiskActiveDict, DiskActiveDict, CurrentReaders);
+				DiskActiveDict -> loop(DiskActiveDict, DiskActiveDict, CurrentReaders);
 				_ ->
 					% write(ActiveDict, ActiveDictFilename),
-					active_loop(ActiveDict, ActiveDict, CurrentReaders)
+					loop(ActiveDict, ActiveDict, CurrentReaders)
 			end
 	end.
-
-% % Tablet server loop
-inactive_loop() ->
-	receive
-		{addInactiveRow, Key, Value} -> laod_inactives_dict_from_disk, add_this_row, write_back_to_disk;
-		% {addActiveRow, Key, Value} -> 
-		% 	NewActiveDict = dict:append(Key, Value, ActiveDict),
-		% 	io:write(dict:to_list(NewActiveDict)),
-		% 	loop(NewActiveDict, DiskActiveDict, CurrentReaders);
-		{deleteInactiveRow, Key} -> todo;
-		% {deleteRow, Key} ->
-		% 	NewActiveDict = dict:erase(Key, ActiveDict),
-		% 	loop(NewActiveDict, DiskActiveDict, CurrentReaders);
-		{updateRowValue, Key, Value} -> todo;
-		{updateRowActive, Key, Active} -> todo
-	% after
-	% 	% this refresh timer is redundant
-	% 	5000 ->
-	% 		case ActiveDict of
-	% 			DiskActiveDict -> loop(DiskActiveDict, DiskActiveDict, CurrentReaders);
-	% 			_ ->
-	% 				% write(ActiveDict, ActiveDictFilename),
-	% 				loop(ActiveDict, ActiveDict, CurrentReaders)
-	% 		end
-	end.
-
-% % Tablet server loop
-loop(ActivePid, InactivePid) ->
-	receive
-		{queryR, Function} -> ActivePid ! {queryR, Function};
-		{queryW, Function} -> ActivePid ! {queryW, Function};
-		{addInactiveRow, Key, Value} -> InactivePid ! {addInactiveRow, Key, Value};
-		{addActiveRow, Key, Value} -> ActivePid ! {addActiveRow, Key, Value};
-		{deleteInactiveRow, Key} -> InactivePid ! {deleteInactiveRow, Key};
-		% {deleteRow, Key} -> 
-		{updateRowValue, Key, Value} -> InactivePid ! {updateRowValue, Key, Value};
-		{updateRowActive, Key, Active} -> InactivePid ! {updateRowActive, Key, Active}
-	end,
-	loop(ActivePid, InactivePid).
 
 wait_for_readers(0) -> ok;
 wait_for_readers(N) ->
