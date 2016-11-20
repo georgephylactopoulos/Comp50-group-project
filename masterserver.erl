85017f024f1ae006efad467459fd5c83dc9f64ab
georgephylactopoulos <george.phylactopoulos@tufts.edu>
2016-11-20 02:05:18 -0500

    Update masterserver.erl

--- masterserver.erl
+++ masterserver.erl
@@ -2,6 +2,10 @@
 -export([start/1,add_tablet/2,update_clients/1,loop/2]).
 
 
+%The way this has been implemented now, we need to call update_clients
+%every time we add a tablet. However, we might as well just make add_tablet
+%call update_clients().
+
 start(MasterServerName) ->
 	Pid = spawn(masterserver,loop,[[],[]]),
 	register(MasterServerName,Pid),
@@ -14,9 +18,10 @@ add_tablet(MasterServerLoopName,Tablet) ->
 update_clients(MasterServerLoopName)-> 
 	{MasterServerLoopName, node()} ! {update_clients}.
 
-update_clients_helper([], TabletList) -> 'Clients Updated';
+update_clients_helper([], _TabletList) -> 'Clients Updated';
 update_clients_helper([H|T],TabletList) ->
-	H ! {tablet_list_update, TabletList}.
+	H ! {tablet_list_update, TabletList},
+	update_clients_helper(T,TabletList).
 
 loop(ClientList,TabletList) -> 
 	receive
