ee5a6dade8152bdb7fad299c1813f30da90fab1f
georgephylactopoulos <george.phylactopoulos@tufts.edu>
2016-11-20 02:02:46 -0500

    Update client.erl

--- client.erl
+++ client.erl
@@ -14,7 +14,7 @@
 start(Name, Master_Registered_Name, Master_Node) ->
 	Pid = spawn(client,loop,[[], Master_Registered_Name, Master_Node]),
 	register(Name,Pid),
-	subscribe_to_the_master(Master_Registered_Name, Master_Node),
+	subscribe_to_the_master(Master_Registered_Name, Master_Node, Name),
 	Pid.
 
 add_row(Loop_process_name,Key,Value) -> 
@@ -23,7 +23,7 @@ add_row(Loop_process_name,Key,Value) ->
 make_queryW(Loop_process_name,Function) -> 
 	{Loop_process_name,node()} ! {queryW, Function}.
 
-send_queryW_message([], Function) -> 'queryW made';
+send_queryW_message([], _Function) -> 'queryW made';
 send_queryW_message([H|T],Function) -> 
 	H ! {queryW,Function},
 	send_queryW_message(T,Function).
@@ -31,13 +31,13 @@ send_queryW_message([H|T],Function) ->
 make_queryR(Loop_process_name,Function) -> 
 	{Loop_process_name,node()} ! {queryR, Function}.
 
-send_queryR_message([], Function) -> 'queryR made';
+send_queryR_message([], _Function) -> 'queryR made';
 send_queryR_message([H|T],Function) -> 
 	H ! {queryR,Function},
 	send_queryR_message(T,Function).
 
 %works
-send_add_row_message(Tablet_list,Master_Name,Master_Node,Key,Value) ->
+send_add_row_message(Tablet_list,Key,Value) ->
 	TabletListLength = length(Tablet_list),
 	Index = rand:uniform(TabletListLength),
 	Tablet = lists:nth(Index,Tablet_list),
@@ -53,7 +53,7 @@ subscribe_to_the_master(Master_Registered_Name, Master_Node,Loop_process_name) -
 loop(Tablet_list, Master_Name,Master_Node) -> 
 	receive
 		{tablet_list_update, NewList} ->  loop(NewList,Master_Name,Master_Node);
-		{add_row, Key, Value} -> send_add_row_message(Tablet_list,Master_Name,Master_Node,Key,Value),
+		{add_row, Key, Value} -> send_add_row_message(Tablet_list,Key,Value),
 								 loop(Tablet_list,Master_Name,Master_Node);
 		{queryW, Function} -> send_queryW_message(Tablet_list,Function),
 							  loop(Tablet_list,Master_Name,Master_Node);
