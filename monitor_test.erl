ecc9f71f9db48b7f34cd8dfe4ef646fe9b9950e9
georgephylactopoulos <george.phylactopoulos@tufts.edu>
2016-12-08 01:12:24 -0500

    monitor module should work

---
+++ monitor_test.erl
@@ -0,0 +1,44 @@
+c(tablet_server).
+c(master_server).
+c(monitor).
+
+{ok,Tablet1} = tablet_server:start().
+'^this_is_tablet1'.
+{ok,Tablet2} = tablet_server:start().
+'^this_is_tablet2'.
+
+Master = master_server:start(master_registered_name).
+
+master_server:add_tablet(Master,Tablet1).
+master_server:add_tablet(Master,Tablet2).
+
+tabletsadded.
+
+monitor:start(1,Master).
+
+monitorstarted.
+
+master_server:print_tablet_list(Master).
+
+exit(Tablet2,reason).
+
+tablet2killed.
+
+timer:sleep(10).
+
+master_server:print_tablet_list(Master).
+
+tablet2shouldnotbeabove.
+
+{ok,Tablet3} = tablet_server:start().
+
+master_server:add_tablet(Master,Tablet3).
+
+exit(Tablet1,reason).
+
+timer:sleep(10).
+
+master_server:print_tablet_list(Master).
+
+now_only_tablet3_should_be_above.
+
