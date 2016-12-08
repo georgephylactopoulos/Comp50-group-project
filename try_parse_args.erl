93f36f072d903451da1c332a80d1868622fb13c2
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2016-12-08 12:45:13 -0500

    try turning strings (command line args) to atoms

--- try_parse_args.erl
+++ try_parse_args.erl
@@ -2,5 +2,9 @@
 % To run this, run
 % erl < try_parse_args.erl -foo bar
 
-{ok, Foo} = init:get_argument(foo).
+{ok, [[Foo]]} = init:get_argument(foo).
 Foo.
+
+'make this an atom'.
+
+list_to_atom(Foo).
