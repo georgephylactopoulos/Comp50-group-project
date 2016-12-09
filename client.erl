2463073ac62d3e995779cb4e221ead6503339062
georgephylactopoulos <george.phylactopoulos@tufts.edu>
2016-12-09 18:28:32 -0500

    Code cleanup (comments, whitespace and 80cols)

--- client.erl
+++ client.erl
@@ -1,33 +1,53 @@
-
 -module(client).
 -export([add_row/3,send_add_row_message/3,delete_row/2,
 	update_row/3,get_row/2,filter/2]).
 
-send_add_row_message([],Key,Value) -> no_tablets;
-send_add_row_message(Tablets,Key,Value) ->
-	TabletListLength = length(Tablets),
-	Index = rand:uniform(TabletListLength),
-	Tablet = lists:nth(Index,Tablets),
-	gen_server:cast(Tablet,{add_row, Key,Value}).
-
-add_row(MasterServerPid,Key,Value) ->
-	Tablets = gen_server:call(MasterServerPid,{get_tablets}),
+% Function asks the master which tablets are currently in the
+% cluster and the send_add_row_message() helper function.
+add_row(MasterServerPid, Key, Value) ->
+	Tablets = gen_server:call(MasterServerPid, {get_tablets}),
 	send_add_row_message(Tablets, Key, Value).
 
-delete_row(MasterServerPid,Key) ->
-    Tablets = gen_server:call(MasterServerPid,{get_tablets}),
+% Helper function for add_row(). Function selects
+% a tablet at random and sends it an add_row command.
+% Random selection should provide more even load 
+% balancing.
+send_add_row_message([], Key, Value) -> no_tablets;
+send_add_row_message(Tablets, Key, Value) ->
+	TabletListLength = length(Tablets),
+	Index = rand:uniform(TabletListLength),
+	Tablet = lists:nth(Index, Tablets),
+	gen_server:cast(Tablet, {add_row, Key, Value}).
+
+% Function asks the master which tablets are currently in the
+% cluster and then sends them delete_row commands in parallel.
+% There should be at most one tablet with an active row that 
+% corresponds to this key. If that active row is found it will
+% be deleted.
+delete_row(MasterServerPid, Key) ->
+    Tablets = gen_server:call(MasterServerPid, {get_tablets}),
 	util:parallel_map(fun(T) ->
 		gen_server:cast(T, {delete_row, Key})
 	end, Tablets).
 
-update_row(MasterServerPid,Key,Value) ->
-	Tablets = gen_server:call(MasterServerPid,{get_tablets}),
+% Function asks the master which tablets are currently in the
+% cluster and then sends them update_row commands in parallel.
+% There should be at most one tablet with an active row that 
+% corresponds to this key. If that active row is found it will
+% be updated.
+update_row(MasterServerPid, Key, Value) ->
+	Tablets = gen_server:call(MasterServerPid, {get_tablets}),
 	util:parallel_map(fun(T) ->
 		gen_server:cast(T, {update_row, Key, Value})
 	end, Tablets).
 
+% Function asks the master which tablets are currently in the
+% cluster and then get_row calls in parallel.
+% There should be at most one tablet with an active row that 
+% corresponds to this key. If that active row is found it will
+% be returned. If no match is found, the function returns no_match
 get_row(MasterServerPid, Key) ->
-	Tablets = gen_server:call(MasterServerPid,{get_tablets}),
+	Tablets = gen_server:call(MasterServerPid, {get_tablets}),
 	Results = util:parallel_map(fun(T) ->
 		Result = gen_server:call(T, {get_row, Key}),
 		case Result of
@@ -40,8 +60,11 @@ get_row(MasterServerPid, Key) ->
 		_ -> no_match
 	end.
 
+% Function asks the master which tablets are currently in the
+% cluster and then sends then makes parallel filter calls.
+% It then merges the results and returns them.
 filter(Master, Function) ->
-	Tablets = gen_server:call(Master,{get_tablets}),
+	Tablets = gen_server:call(Master, {get_tablets}),
 	Results = util:parallel_map(fun(T) ->
 		Result = gen_server:call(T, {filter, Function})
 	end, Tablets),
--- master_server.erl
+++ master_server.erl
@@ -1,9 +1,9 @@
-
 -module(master_server).
 
 -export([start/1, stop/1]).
 -export([add_tablet/2,delete_tablet/2,print_tablet_list/1, find_master/2]).
 -export([init/1, handle_call/3, handle_cast/2]).
+-behaviour(gen_server).
 
 start(Name) ->
 	{ok, Pid} = gen_server:start(master_server, [], []),
@@ -14,6 +14,8 @@ start(Name) ->
 stop(Pid) ->
 	gen_server:stop(Pid).
 
+%Exposed functions
+
 find_master(MasterNode, MasterName) ->
 	rpc:call(MasterNode, erlang, whereis, [MasterName]).
 
@@ -23,14 +25,13 @@ add_tablet(Pid, Tablet) ->
 delete_tablet(Pid, Tablet) -> 
 	gen_server:cast(Pid, {delete_tablet, Tablet}).
 
-% for debugging purposes
+%%for debugging purposes
 print_tablet_list(Pid) -> 
 	gen_server:cast(Pid, {print_tablet_list}).
 
+
 % Gen Server behavior code
-
 % Tablets are represented by their pids
-
 init(_) -> {ok, []}.
 
 handle_cast({is_down,Tablet}, Tablets) ->
@@ -48,10 +49,10 @@ handle_cast({print_tablet_list}, Tablets) ->
 	print_list_helper(Tablets),
 	{noreply, Tablets}.
 
-handle_call({get_tablets}, _Sender, Tablets) ->
+handle_call({get_tablets}, Sender, Tablets) ->
 	{reply, Tablets, Tablets}.
 
-% for debugging purposes
+%%for debugging purposes
 print_list_helper([]) -> [];
 print_list_helper([H|T]) -> io:format("printing: ~p~n", [H]),
 						    print_list_helper(T).
